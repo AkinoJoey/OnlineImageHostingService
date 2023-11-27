8669c4018ab3b3b3eecae41e22db7fbd9670f64a
AkinoJoey <gakiya213@gmail.com>
2023-11-27 16:11:50 +0900

    mimeを追加

--- Database/Migrations/2023-11-26_1700962691_CreateImageTable.php
+++ Database/Migrations/2023-11-26_1700962691_CreateImageTable.php
@@ -14,6 +14,7 @@ class CreateImageTable implements SchemaMigration
                 path VARCHAR(255) NOT NULL UNIQUE,
                 shared_url VARCHAR(255) NOT NULL UNIQUE,
                 delete_url VARCHAR(255) NOT NULL UNIQUE,
+                mime VARCHAR(20) NOT NULL,
                 view_count INT DEFAULT 0);'
         ];
     }
