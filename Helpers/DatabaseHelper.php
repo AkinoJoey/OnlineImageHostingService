4e298f33e836a007353697651a0adaf35ac94312
AkinoJoey <gakiya213@gmail.com>
2023-11-27 16:12:29 +0900

    mimeをスキーマに追加することによる変更

--- Helpers/DatabaseHelper.php
+++ Helpers/DatabaseHelper.php
@@ -6,11 +6,11 @@ use Database\MySQLWrapper;
 use Exception;
 
 class DatabaseHelper{
-    public static function insertImageData(string $path, string $shared_url, string $delete_url): bool{
+    public static function insertImageData(string $path, string $shared_url, string $delete_url, string $mime): bool{
         $mysqli = new MySQLWrapper();
-        $query = "INSERT INTO images (path, shared_url, delete_url) VALUES (?, ?, ?)";
+        $query = "INSERT INTO images (path, shared_url, delete_url, mime) VALUES (?, ?, ?, ?)";
         $stmt = $mysqli->prepare($query);
-        $stmt->bind_param("sss",$path, $shared_url, $delete_url);
+        $stmt->bind_param("ssss",$path, $shared_url, $delete_url, $mime);
         $result = $stmt->execute();
 
         if (!$result) throw new Exception("Error executing INSERT query: " . $stmt->error);
@@ -20,7 +20,7 @@ class DatabaseHelper{
 
     public static function getImageData(string $shared_url): Array | false{
         $db = new MySQLWrapper();
-        $stmt = $db->prepare("SELECT path, view_count FROM images WHERE shared_url = ?");
+        $stmt = $db->prepare("SELECT path, mime ,view_count FROM images WHERE shared_url = ?");
         $stmt->bind_param('s', $shared_url);
         $stmt->execute();
         $result = $stmt->get_result();
