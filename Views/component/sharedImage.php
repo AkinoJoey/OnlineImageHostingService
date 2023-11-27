243b714d42f230fbded1e9e119b24e73e7e26ff3
AkinoJoey <gakiya213@gmail.com>
2023-11-27 17:26:33 +0900

    画像を中央寄せにするためのクラスを追加

--- Views/component/sharedImage.php
+++ Views/component/sharedImage.php
@@ -2,7 +2,7 @@
     <h2 class="text-align-center"><a class="text-decoration-none" href="/">Pix Place</a></h2>
     <div>
         <p class="text-align-center"><?= $viewCount ?></p>
-        <div>
+        <div class="text-align-center">
             <img src="data:<?= $mime ?>;base64,<?= $image ?>" alt="uploaded image">
         </div>
     </div>
