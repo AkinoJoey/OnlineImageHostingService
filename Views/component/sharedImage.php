c72a5fe4463692d43b43faf450f03dfe81ded5f0
AkinoJoey <gakiya213@gmail.com>
2023-11-28 15:44:01 +0900

    eye iconを追加

--- Views/component/sharedImage.php
+++ Views/component/sharedImage.php
@@ -1,7 +1,10 @@
 <main class="container">
-    <h2 class="text-align-center"><a class="text-decoration-none" href="/">Pix Place</a></h2>
+    <h2 class="text-align-center mb-0"><a class="text-decoration-none" href="/">Pix Place</a></h2>
     <div>
-        <p class="text-align-center"><?= $viewCount ?></p>
+        <div class="row center-xs mb-3">
+            <img src="/images/eye.svg" alt="eye image" width="25" , height="30" title="閲覧回数">
+            <p class="mb-0"><?= $viewCount ?></p>
+        </div>
         <div class="text-align-center">
             <img src="data:<?= $mime ?>;base64,<?= $image ?>" alt="uploaded image">
         </div>
