13172817c81143e542404f97179fb8281c7b68a8
AkinoJoey <gakiya213@gmail.com>
2023-11-29 12:51:09 +0900

    isFileSizeSmallerThan5MB関数を追加

--- Helpers/ValidationHelper.php
+++ Helpers/ValidationHelper.php
@@ -5,8 +5,14 @@ namespace Helpers;
 class ValidationHelper
 {
     public static function validateFileType(string $mime) : bool{
-        $allowedMimeTypes = ['image/png', 'image/jpg', 'image/gif'];
+        $allowedMimeTypes = ['image/png', 'image/jpeg', 'image/gif'];
         return in_array($mime, $allowedMimeTypes);
+    }
+
+    public static function isFileSizeSmallerThan5MB(string $filePath) : bool {
+        $fileSize = filesize($filePath);
+        $isSmallerThan5MB = $fileSize < (5 * 1024 * 1024); // 5MBをバイトに変換
+        return $isSmallerThan5MB;
     }
 
 
