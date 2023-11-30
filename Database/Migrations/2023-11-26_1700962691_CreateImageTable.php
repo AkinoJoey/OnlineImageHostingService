68aed9cf9f4ff1b5bd42d8a769b5ab81715cb266
AkinoJoey <gakiya213@gmail.com>
2023-11-30 17:51:50 +0900

    サイズ、IPアドレス、作成日を追加

--- Database/Migrations/2023-11-26_1700962691_CreateImageTable.php
+++ Database/Migrations/2023-11-26_1700962691_CreateImageTable.php
@@ -12,10 +12,13 @@ class CreateImageTable implements SchemaMigration
             'CREATE TABLE IF NOT EXISTS images(
                 id INT PRIMARY KEY AUTO_INCREMENT, 
                 path VARCHAR(255) NOT NULL UNIQUE,
+                byte_size int NOT NULL,
                 shared_url VARCHAR(255) NOT NULL UNIQUE,
                 delete_url VARCHAR(255) NOT NULL UNIQUE,
                 mime VARCHAR(20) NOT NULL,
                 view_count INT DEFAULT 0,
+                uploaded_ip_address VARCHAR(45) NOT NULL,
+                created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
                 last_accessed_at DATETIME DEFAULT CURRENT_TIMESTAMP);'
         ];
     }
