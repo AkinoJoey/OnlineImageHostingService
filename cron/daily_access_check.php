85352bc1fc349d8d6b64ecf4dfa3fea30f0c235a
AkinoJoey <gakiya213@gmail.com>
2023-12-03 18:33:22 +0900

    パスの修正

--- cron/daily_access_check.php
+++ cron/daily_access_check.php
@@ -1,10 +1,21 @@
 <?php
+# cronでは作業ディレクトリがホームディレクトリになるから、作業ディレクトリをcronディレクトリに変更
+chdir(__DIR__);
+
+
+require('../autoload.php');
 
 use Helpers\DatabaseHelper;
 
 $oldData = DatabaseHelper::getInactiveImageData30Days();
 
+$uploadDir = '../uploads/';
+
 if(!empty($oldData)){
     DatabaseHelper::deleteInactiveImageData30Days();
-    array_map('unlink', $oldData);
+    for($i = 0; $i < count($oldData); $i++){
+        $currentData = $uploadDir . $oldData[$i];
+        unlink($currentData);
+    }
 }
+
